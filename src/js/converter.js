2f9f1bf4d08369569ecdcb5bed4467fd048e1cae
marcinbrylka <marcin_brylka@wp.pl>
2018-06-26 14:13:38 +0200

    initial commit

---
+++ src/js/converter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Converter extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Converter;
